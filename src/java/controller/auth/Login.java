5717c6bc32126f7f012fd0cd9834fef4c4110c9a
Vũ Đức Tiến <thunderfalcon2001@gmail.com>
2022-01-13 17:16:31 +0700

    Merge origin/kytd/initBase into kytd/initBase
    Conflicts:
    	web/WEB-INF/web.xml

---
+++ src/java/controller/auth/Login.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.auth;
+
+import dal.AccountDBContext;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.Account;
+
+/**
+ *
+ * @author Vu Duc Tien
+ */
+public class Login extends HttpServlet {
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String pass = request.getParameter("password");
+        AccountDBContext adbc = new AccountDBContext();
+        
+        Account account = adbc.getAccount(username, pass);
+        
+        if (account == null) {
+            request.setAttribute("isFail", true);
+            request.setAttribute("account", null);
+            request.setAttribute("user", null);
+            request.getRequestDispatcher("View/auth/login.jsp").forward(request, response);
+        } else {
+            HttpSession session = request.getSession();
+            session.setAttribute("account", account);
+            response.sendRedirect("home");
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
