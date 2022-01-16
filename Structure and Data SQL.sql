0b2e1a4733c9fb76fa0215f921b173f00bcff48b
kytdhe151114 <kytdhe151114@fpt.edu.vn>
2022-01-16 21:10:42 +0700

    add SQL code and data for project

--- Structure and Data SQL.sql
+++ Structure and Data SQL.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `quiz_practice_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `quiz_practice_db` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `quiz_practice_db`;
 -- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
 --
@@ -29,8 +29,9 @@ CREATE TABLE `account` (
   `username` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
   `account_status` varchar(45) NOT NULL,
-  PRIMARY KEY (`account_id`,`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+  PRIMARY KEY (`account_id`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,8 +40,36 @@ CREATE TABLE `account` (
 
 LOCK TABLES `account` WRITE;
 /*!40000 ALTER TABLE `account` DISABLE KEYS */;
-INSERT INTO `account` VALUES (1,'admin','123','ACTIVE'),(2,'test1','123','DEACTIVE'),(3,'test12','123','ACTIVE');
+INSERT INTO `account` VALUES (1,'admin','admin123','ACTIVE'),(2,'user','user123','ACTIVE'),(3,'staff','staff123','ACTIVE'),(12,'test','test123','ACTIVE');
 /*!40000 ALTER TABLE `account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `account_profile`
+--
+
+DROP TABLE IF EXISTS `account_profile`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `account_profile` (
+  `account_id` int NOT NULL,
+  `account_email` varchar(45) NOT NULL,
+  `account_phone` varchar(45) NOT NULL,
+  `account_fullname` varchar(45) NOT NULL,
+  `address` varchar(45) NOT NULL,
+  PRIMARY KEY (`account_id`),
+  CONSTRAINT `FK_account_account_profile_id` FOREIGN KEY (`account_id`) REFERENCES `account` (`account_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `account_profile`
+--
+
+LOCK TABLES `account_profile` WRITE;
+/*!40000 ALTER TABLE `account_profile` DISABLE KEYS */;
+INSERT INTO `account_profile` VALUES (1,'admin@admin.com','0922228888','Nguyen Van Admin','Admin Town'),(2,'user@user.com','0966664444','Tran Thi User','Mar'),(3,'staff@staff.com','0233335555','Le Minh Staff','SWP'),(12,'tester@tester.com','0222244442','Tester Không Cay Cú','tester làng');
+/*!40000 ALTER TABLE `account_profile` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -57,9 +86,8 @@ CREATE TABLE `setting` (
   `setting_description` varchar(500) NOT NULL,
   `setting_value` varchar(45) NOT NULL,
   `setting_status` varchar(45) NOT NULL,
-  PRIMARY KEY (`setting_id`),
-  UNIQUE KEY `settiing_name_UNIQUE` (`setting_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+  PRIMARY KEY (`setting_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,7 +96,7 @@ CREATE TABLE `setting` (
 
 LOCK TABLES `setting` WRITE;
 /*!40000 ALTER TABLE `setting` DISABLE KEYS */;
-INSERT INTO `setting` VALUES (1,'c','b','b','b','ACTIVE'),(2,'b','b','b','b','DEACTIVE'),(3,'d','d','d','d','ACTIVE'),(4,'e','e','e','e','DEACTIVE');
+INSERT INTO `setting` VALUES (1,'Adminnnnnnnnnnnnnnn','Role','Chức vụ cao trọng, cầm đầu dự án','Administrator','DEACTIVE'),(2,'Teacher','Role','Tạo bài về nhà cho học sinh','Giáo viên','ACTIVE'),(3,'Student','Role','Làm bài tập và luyện tập','Học sinh','ACTIVE'),(4,'Math','Subject','Môn học','Toán học','DEACTIVE'),(5,'History','Subject','Môn học về những giá trị mà cha ông ta đã để lại','Lịch Sử','ACTIVE'),(6,'Biology','Subject','Môn học thụ phấn đậu Hà Lan và lai ruồi','Sinh học','ACTIVE'),(7,'QnA','Category','Nơi trao đổi giữa các người dùng','Hỏi đáp','ACTIVE'),(8,'DIY','Category','Chia sẻ các mẹo trong học tập','Daily Hack','ACTIVE'),(9,'Report','Category','Nơi báo cáo những tài khoản không theo chính sách của trang','Báo cáo','ACTIVE'),(10,'Test','Role','Test Setting','Testttttt','ACTIVE');
 /*!40000 ALTER TABLE `setting` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -81,4 +109,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-01-13 19:59:52
+-- Dump completed on 2022-01-16 21:06:08
